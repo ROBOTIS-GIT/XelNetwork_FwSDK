3dc7d7021186da7b56621e13b968b4afff77fb36
Kei <kkw@robotis.com>
2018-08-28 18:04:44 +0900

    Added structure for xel network.

---
+++ common/xel/xel_struct.hpp
@@ -0,0 +1,91 @@
+/*
+ * xel_net_struct.hpp
+ *
+ *  Created on: Aug 23, 2018
+ *      Author: kei
+ */
+
+#ifndef XEL_NET_STRUCT_HPP_
+#define XEL_NET_STRUCT_HPP_
+
+#include "stdint.h"
+#include "xel/xel_data_struct.h"
+
+namespace XelNetwork
+{
+
+enum DataDirection
+{
+  SEND = 0,
+  RECEIVE
+};
+
+enum XelStatus
+{
+  NOT_CONNECTTED = 0,
+  NEW_CONNECTION,
+  LOST_CONNECTION,
+  RUNNING
+};
+
+enum DataType
+{
+  BOOLEAN = 0,
+  CHAR,
+  INT8,
+  UINT8,
+  INT16,
+  UINT16,
+  INT32,
+  UINT32,
+  INT64,
+  UINT64,
+  FLOAT32,
+  FLOAT64,
+  VECTOR3,
+  QUATERNION,
+  POINT,
+  TWIST,
+  IMU
+};
+
+struct XelHeader_t
+{
+  uint8_t              xel_id;
+  XelNetwork::DataType data_type;
+  uint32_t             data_get_interval_hz;
+  char                 data_name[32];         //ROS2 topic name
+  uint8_t              msg_type;              //ros2 message type (topic, service, action..)
+  uint16_t             data_addr;
+  uint8_t              data_lenght;
+};
+
+struct XelStatus_t
+{
+  XelStatus  previous;
+  XelStatus  current;
+  bool       flag_changed;
+  bool       flag_get_data;
+};
+
+struct XelDDS_t
+{
+  char    msg_name[32];         //DDS topic name
+  uint8_t msg_type;             //ROS2 message type (topic, service, action..)
+  void(*p_callback_func)(void* msg, void* arg);
+};
+
+typedef struct XelInfo
+{
+  struct XelStatus_t status;
+  struct XelHeader_t header;
+  uint8_t            data[128];
+  struct XelDDS_t    dds;
+} XelInfo_t;
+
+
+
+
+} //XelNetwork
+
+#endif /* XEL_NET_STRUCT_HPP_ */
